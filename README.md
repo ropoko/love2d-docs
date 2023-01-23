56de6931b0e1f8116f521e71d1f29302d1ab128c
ropoko <rodrigostramantinoli@gmail.com>
2023-01-23 14:38:48 -0300

    feat: removed blog

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Love2D Docs
 
-Love2D docs rewrite using Docussaurus using [Love API](https://github.com/love2d-community/love-api)
+Love2D docs rewrite using Docussaurus using [Love Community API](https://github.com/love2d-community/love-api)
+
+
+#  Updates
+for now the updates will be made manually, following the steps:
+
+1. import the repo (master branch)
+```
+git clone https://github.com/love2d-community/love-api
+```
+
+2. asdas
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,3 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -13,11 +10,6 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ropoko', // Usually your GitHub org/user name.
-  projectName: 'love2d-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
