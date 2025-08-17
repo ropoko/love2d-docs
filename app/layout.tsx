ebef691b02b472f6281fc122d9b880a24b561fe1
ropoko <rodrigostramantinoli@gmail.com>
2025-08-17 20:17:26 -0300

    feat: new search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
-import { Layout } from 'nextra-theme-docs'
-import 'nextra-theme-docs/style.css'
-import { getPageMap } from 'nextra/page-map'
+import { Layout, Navbar } from "nextra-theme-docs";
+import { getPageMap } from "nextra/page-map";
+
+import "nextra-theme-docs/style.css";
 
 export const metadata = {
-	title: 'Love2D Docs',
-	description: 'Documentation for Love2D',
-}
+	title: "Love2D Docs",
+	description: "Documentation for Love2D",
+};
 
-export default async function MyLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <Layout
-          pageMap={await getPageMap()}
-          sidebar={{ autoCollapse: true }}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-        >
-          {children}
-        </Layout>
-      </body>
-    </html>
-  )
+export default async function MyLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" dir="ltr" suppressHydrationWarning>
+			<body>
+				<Layout
+					navbar={<Navbar logo={<b>Love2D Docs</b>} />}
+					pageMap={await getPageMap()}
+					sidebar={{ autoCollapse: true }}
+					docsRepositoryBase="https://github.com/ropoko/love2d-docs"
+				>
+					{children}
+				</Layout>
+			</body>
+		</html>
+	);
 }
